daeb2aa300ffcc25567618e4bbda392fd6b68afd
Stevee205 <50575266+Stevee205@users.noreply.github.com>
2020-04-03 12:03:27 +0200

    added System3

---
+++ QualitaetsSoftware/src/System3.java
@@ -0,0 +1,23 @@
+public class System3 extends ASystem{
+
+    public System3(int numberOfComponents, double[] lambdaI) {
+        super(numberOfComponents, lambdaI);
+    }
+
+    public double getRP(){
+        double FP = this.getFP();
+        double RP = 1 - FP;
+        return RP;
+    }
+
+    public double getFP(){
+        double interimResult=1;
+        double euler = Math.E;
+        for (int i = 0; i < lambdaI.length; i++) {
+            double reliability = Math.pow(e, (-lambdaI[i]*87600));
+            double difference = 1 - reliability;
+            interimResult  = interimResult * difference;
+        }
+        return interimResult;
+    }
+}
